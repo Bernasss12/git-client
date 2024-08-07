cddcbc8db4fb0ad5e85a9dbc914f0f92ace060c4
Bernardo Antunes <bernasss12@gmail.com>
2024-08-08 00:49:46 +0100

    remove check

--- src/main/kotlin/model/GitObject.kt
+++ src/main/kotlin/model/GitObject.kt
@@ -31,7 +31,7 @@ sealed class GitObject : Printable {
                 val bytes = it.readAllBytes()
                 val (headerBytes, contentBytes) = splitHeaderAndContent(bytes)
                 val (type, length) = headerBytes.asString().split(" ", limit = 2)
-                check(contentBytes.size == length.toInt()) { "Content doesn't have the size defined in header: ${contentBytes.size} != $length" }
+                //check(contentBytes.size == length.toInt()) { "Content doesn't have the size defined in header: ${contentBytes.size} != $length" }
                 return when(type) {
                     Blob.TYPE -> Blob(contentBytes)
                     Tree.TYPE -> Tree(contentBytes)
